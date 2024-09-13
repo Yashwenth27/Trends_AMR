fbfe9b1a699b6657f7ffdeb41ab47ed1e6bd70a4
Yashwenth S <127986257+Yashwenth27@users.noreply.github.com>
2024-09-13 12:14:58 +0530

    Update getresource.py

--- getresource.py
+++ getresource.py
@@ -3,31 +3,9 @@ import os
 import zipfile
 
 def download_and_extract_with_gdown(gdrive_link, folder_name, download_path, extract_path):
-    zip_path = os.path.join(download_path, folder_name + '.zip').replace("\\","/")
-
-    # Check if the folder is already extracted
-    if os.path.exists(extract_path):
-        print(f"Folder '{folder_name}' already exists, skipping download.")
-        return
-
-    # If the zip file exists, check if it's a valid zip file
-    if os.path.exists(zip_path):
-        try:
-            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-                zip_ref.testzip()  # Test if it's a valid zip
-            print(f"Zip file '{folder_name}.zip' already exists and is valid.")
-        except zipfile.BadZipFile:
-            print(f"Zip file '{folder_name}.zip' is invalid or corrupted, redownloading...")
-            os.remove(zip_path)
-            gdown.download(gdrive_link, zip_path, quiet=False)
-    else:
-        print(f"Downloading '{folder_name}' from Google Drive...")
-        gdown.download(gdrive_link, zip_path, quiet=False)
-        print(f"Downloaded '{folder_name}.zip' to {zip_path}")
-
-    # Extract the zip file
+    zip_path = "Final Data-20240913T063708Z-001.zip"
     print(f"Extracting '{folder_name}.zip'...")
-    print(zip_path)
+    print("")
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_path)
     print(f"Extracted to '{extract_path}'")
