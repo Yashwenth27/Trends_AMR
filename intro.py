1a8a14e87c1c7d62ab353d5f41388a080f0a8d9c
Yashwenth S <127986257+Yashwenth27@users.noreply.github.com>
2024-09-14 17:58:59 +0530

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,73 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+st.write('<style>div.block-container{padding-top:0rem;}</style>', unsafe_allow_html=True)
+
+# Navbar HTML
+navbar_html = '''
+<style>
+    .st-emotion-cache-12fmjuu{
+        z-index: 100;
+    }
+    .st-emotion-cache-h4xjwg{
+        z-index: 100;
+    }
+    h2{
+    color: white;
+    }
+    .css-hi6a2p {padding-top: 0rem;}
+    .navbar {
+        background-color: #355E3B;
+        padding: 0.3rem;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: 1000;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .navbar .logo {
+        display: flex;
+        align-items: center;
+    }
+    .navbar .logo img {
+        height: 40px;
+        margin-right: 10px;
+    }
+    .navbar .menu {
+        display: flex;
+        gap: 1.5rem;
+    }
+    .navbar .menu a {
+        color: white;
+        font-size: 1.2rem;
+        text-decoration: none;
+    }
+    .content {
+        padding-top: 5rem;  /* Adjust this based on navbar height */
+    }
+</style>
+
+<nav class="navbar">
+    <div class="logo">
+        <h2 id="hh">MERIT Dashboard</h2>
+    </div>
+    <div class="menu">
+        <a href="">Home</a>
+        <a href="https://merit-dashboard.streamlit.app/" target="_self">Dataset</a>
+        <a href="https://merit-mining.streamlit.app/" target="_self">Association Mining</a>
+        <a href="https://mining-query.streamlit.app/" target="_self">Track MDR</a>
+        <a href="https://merit-dataset.streamlit.app/" target="_self">Antimicrobial Trend</a>
+        <a href="">About Us</a>
+    </div>
+</nav>
+
+<div class="content">
+'''
+
+# Injecting the navigation bar and content padding into the Streamlit app
+st.markdown(navbar_html, unsafe_allow_html=True)
+
+st.header("Intro Page")
